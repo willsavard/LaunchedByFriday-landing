0904a7e27b3b3b3e454dd57dbaa430688a38baed
willsavard <willsavard@gmail.com>
2025-05-27 12:03:40 -0400

    Update layout.tsx
    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: "LaunchedByFriday",
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   )
 }
